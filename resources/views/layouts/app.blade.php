bda47cb76508421328a856146a5e1df0f3c29dee
Nenad Savkic <58524230+Nenadsavkic@users.noreply.github.com>
2022-01-04 00:32:54 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
+    <link rel="icon" href="{{ asset('/images/logo_image/bloggerman2.png') }}" type="image/x-icon" >
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
